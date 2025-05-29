2285b7dfb79c0579f9a10a6ded1a170bbcd37a24
shingo405nagano <makishin.nagano@gmail.com>
2025-05-29 13:43:39 +0000

    [Add] 各セルの座標と、セル内の情報を取得してDataFrame化する関数を追加。

--- utils/raster_info.py
+++ utils/raster_info.py
@@ -1,3 +1,7 @@
+from typing import Optional
+
+import geopandas as gpd
+import numpy as np
 from osgeo import gdal
 import pyproj
 import shapely
@@ -7,7 +11,9 @@ from gdal_drawer.utils.geometry import (
     estimate_utm_crs_from_geometry,
     reprojection_geometry
 )
-from gdal_drawer.utils.config import Bounds, CellSize
+from gdal_drawer.utils.config import (
+    Bounds, CellSize, crs_checker, CRS, XY, Coordinates
+)
 
 
 def get_bounds(dst: gdal.Dataset) -> Bounds:
@@ -32,6 +38,51 @@ def get_bounds(dst: gdal.Dataset) -> Bounds:
     x_max = x_min + cols * transform[1]
     y_min = y_max + rows * transform[-1]
     return Bounds(x_min, y_min, x_max, y_max)
+
+
+def get_reprojected_bounds(dst: gdal.Dataset, out_crs: CRS) -> Bounds:
+    """
+    ## Summary:
+        Get the reprojected bounds of the gdal.Dataset.
+    Args:
+        dst (gdal.Dataset): 
+            gdal.Dataset
+        out_crs (pyproj.CRS): 
+            Output CRS. CRS is a pyproj.CRS object or EPSG code.
+    Returns:
+        Bounds(NamedTuple): (x_min, y_min, x_max, y_max)
+    """
+    bounds = get_bounds(dst)
+    geometry = shapely.box(*bounds)
+    reprojected_geometry = reprojection_geometry(
+        geometry=geometry,
+        in_crs=dst.GetProjection(),
+        out_crs=out_crs
+    )
+    return Bounds(*reprojected_geometry.bounds)
+
+
+def get_center_from_dataset(dst: gdal.Dataset, out_crs: Optional[CRS] = None) -> XY:
+    """
+    ## Summary:
+        Get the center coordinates of the gdal.Dataset.
+    Args:
+        dst (gdal.Dataset): 
+            gdal.Dataset
+        out_crs (pyproj.CRS, optional):
+            Output CRS. If None, the original CRS of the dataset is used.
+            CRS is a pyproj.CRS object or EPSG code.
+    Returns:
+        XY(NamedTuple): 
+            (x, y)
+    """
+    if out_crs is None:
+        bounds = get_bounds(dst)
+    else:    
+        bounds = get_reprojected_bounds(dst, out_crs)
+    x_center = (bounds.x_min + bounds.x_max) / 2
+    y_center = (bounds.y_min + bounds.y_max) / 2
+    return XY(x=x_center, y=y_center)
 
 
 def estimate_utm_crs_from_datasets(
@@ -133,4 +184,116 @@ def resolution_from_dataset(
     x_resol = round(x_len / dst.RasterXSize, digit)
     y_resol = round(y_len / dst.RasterYSize, digit)
     return CellSize(x=x_resol, y=y_resol)
+
+
+def cells_center_coordinates(dst: gdal.Dataset) -> Coordinates:
+    """
+    ## Summary:
+        Get the center coordinates of each cell in the raster dataset.
+    Args:
+        dst (gdal.Dataset): 
+            gdal.Dataset
+    Returns:
+        Coordinates(dataclass):
+            1. X (np.ndarray): shape is (RasterYSize, RasterXSize)
+            2. Y (np.ndarray): shape is (RasterYSize, RasterXSize)
+    """
+    bounds = get_bounds(dst)
+    transform = dst.GetGeoTransform()
+    x_resol = transform[1]
+    y_resol = abs(transform[5])
+    # Half is used to get the center of the cell.
+    half = 0.5
+    # Create a meshgrid of coordinates
+    X = np.arange(bounds.x_min, bounds.x_max, x_resol) + x_resol * half
+    Y = np.arange(bounds.y_min, bounds.y_max, y_resol) + y_resol * half
+    return Coordinates(*np.meshgrid(X, Y))
+
+
+def cells_upper_left_corner_coordinates(dst: gdal.Dataset) -> Coordinates:
+    """
+    ## Summary:
+        Get the upper left corner coordinates of each cell in the raster dataset.
+    Args:
+        dst (gdal.Dataset): 
+            gdal.Dataset
+    Returns:
+        Coordinates(dataclass):
+            1. X (np.ndarray): shape is (RasterYSize, RasterXSize)
+            2. Y (np.ndarray): shape is (RasterYSize, RasterXSize)
+    """
+    bounds = get_bounds(dst)
+    transform = dst.GetGeoTransform()
+    x_resol = transform[1]
+    y_resol = abs(transform[5])
+    # Create a meshgrid of coordinates
+    X = np.arange(bounds.x_min, bounds.x_max, x_resol)
+    Y = np.arange(bounds.y_min, bounds.y_max, y_resol)
+    return Coordinates(*np.meshgrid(X, Y))
+    
+    
+def cells_upper_right_corner_coordinates(dst: gdal.Dataset) -> Coordinates:
+    """
+    ## Summary:
+        Get the upper right corner coordinates of each cell in the raster dataset.
+    Args:
+        dst (gdal.Dataset): 
+            gdal.Dataset
+    Returns:
+        Coordinates(dataclass):
+            1. X (np.ndarray): shape is (RasterYSize, RasterXSize)
+            2. Y (np.ndarray): shape is (RasterYSize, RasterXSize)
+    """
+    bounds = get_bounds(dst)
+    transform = dst.GetGeoTransform()
+    x_resol = transform[1]
+    y_resol = abs(transform[5])
+    # Create a meshgrid of coordinates
+    X = np.arange(bounds.x_min + x_resol, bounds.x_max + x_resol, x_resol)
+    Y = np.arange(bounds.y_min, bounds.y_max, y_resol)
+    return Coordinates(*np.meshgrid(X, Y))
+
+
+def cells_lower_left_corner_coordinates(dst: gdal.Dataset) -> Coordinates:
+    """
+    ## Summary:
+        Get the lower left corner coordinates of each cell in the raster dataset.
+    Args:
+        dst (gdal.Dataset): 
+            gdal.Dataset
+    Returns:
+        Coordinates(dataclass):
+            1. X (np.ndarray): shape is (RasterYSize, RasterXSize)
+            2. Y (np.ndarray): shape is (RasterYSize, RasterXSize)
+    """
+    bounds = get_bounds(dst)
+    transform = dst.GetGeoTransform()
+    x_resol = transform[1]
+    y_resol = abs(transform[5])
+    # Create a meshgrid of coordinates
+    X = np.arange(bounds.x_min, bounds.x_max, x_resol)
+    Y = np.arange(bounds.y_min + y_resol, bounds.y_max + y_resol, y_resol)
+    return Coordinates(*np.meshgrid(X, Y))
+
+
+def cells_lower_right_corner_coordinates(dst: gdal.Dataset) -> Coordinates:
+    """
+    ## Summary:
+        Get the lower right corner coordinates of each cell in the raster dataset.
+    Args:
+        dst (gdal.Dataset): 
+            gdal.Dataset
+    Returns:
+        Coordinates(dataclass):
+            1. X (np.ndarray): shape is (RasterYSize, RasterXSize)
+            2. Y (np.ndarray): shape is (RasterYSize, RasterXSize)
+    """
+    bounds = get_bounds(dst)
+    transform = dst.GetGeoTransform()
+    x_resol = transform[1]
+    y_resol = abs(transform[5])
+    # Create a meshgrid of coordinates
+    X = np.arange(bounds.x_min + x_resol, bounds.x_max + x_resol, x_resol)
+    Y = np.arange(bounds.y_min + y_resol, bounds.y_max + y_resol, y_resol)
+    return Coordinates(*np.meshgrid(X, Y))
 
